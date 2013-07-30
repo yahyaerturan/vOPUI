8fa235fc3bacdcd878138b33b60fad5dfbe0c3d3
Yahya Erturan <root@yahyaerturan.com>
2013-07-30 17:24:59 +0300

    base_url updated once again...

--- index.php
+++ index.php
@@ -5,7 +5,6 @@
   } else {
     $base_url  = ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == "on") ? "https" : "http");
     $base_url  .= "://".$_SERVER['HTTP_HOST'];
-    $base_url  .= str_replace(basename($_SERVER['SCRIPT_NAME']),"",$_SERVER['SCRIPT_NAME']);
   }
 ?>
 <html>
